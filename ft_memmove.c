36a7785f784bcd1c63482909f08d119a5ce13a5c
Marc Casanovas Ventura <macasano@car5s5.42barcelona.com>
2022-11-07 21:38:16 +0100

    Monday 7/11

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: macasano <macasano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/04 20:18:44 by macasano          #+#    #+#             */
-/*   Updated: 2022/11/07 21:08:35 by macasano         ###   ########.fr       */
+/*   Created: 2022/11/07 21:12:31 by macasano          #+#    #+#             */
+/*   Updated: 2022/11/07 21:36:18 by macasano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft.h";
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void * ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
-
-	if (!dst && !src)
-		return (0);
-	i = 0;
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dst);
+	
 }
